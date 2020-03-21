f05b3877b5aa5d75c00119ff60ff053f15207cb9
aomsw13 <54433874+aomsw13@users.noreply.github.com>
2020-03-22 01:10:52 +0700

    update ContactFragment.kt
    add code for sending feedback to firebase after cliking button

--- app/src/main/java/com/example/penquiz/ui/contactus/ContactFragment.kt
+++ app/src/main/java/com/example/penquiz/ui/contactus/ContactFragment.kt
@@ -1,26 +1,39 @@
 package com.example.penquiz.ui.contactus
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.penquiz.R
+import com.firebase.ui.auth.data.model.User
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
-class ContactFragment  : Fragment(), OnMapReadyCallback {
+class ContactFragment  : Fragment(), OnMapReadyCallback,View.OnClickListener {
     private lateinit var contactViewModel: ContactViewModel
     private var googlemapView: MapView? = null
+    private lateinit var auth: FirebaseAuth
+    //private lateinit var database: DatabaseReference
+    private lateinit var textFeedback: EditText
+    private lateinit var postFeedback: Button
+
+    var data_base = FirebaseDatabase.getInstance()
+    var myRef: DatabaseReference = data_base.getReference()
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -42,10 +55,30 @@ class ContactFragment  : Fragment(), OnMapReadyCallback {
         googlemapView?.onCreate(savedInstanceState)
         googlemapView?.getMapAsync(this);
 
+        textFeedback = root.findViewById(R.id.text_feedback)
+        postFeedback= root.findViewById(R.id.post_feedback)
+        //database = FirebaseDatabase.getInstance().reference //point to the root named "penquiz3d349"
+        //database = FirebaseDatabase.getInstance().getReference();
+        postFeedback.setOnClickListener(this)
+
+//        postFeedback.setOnClickListener(View.OnClickListener {
+//            fun onClick(v: View){
+//                val message: String = textFeedback.getText().toString()
+//                if (TextUtils.isEmpty(message)){
+//                    textFeedback.setError("Enter Your Message");
+//                    textFeedback.requestFocus();
+//                    return;
+//                }
+//                database.child("Messages").child("senderID").child(senderID.toString()).child(message).push()
+//
+//            }
+//        })
+
         return root
 //// Inflate the layout for this fragment
 ////return inflater.inflate(R.layout.fragment_contactus, container, false)
     }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
 
@@ -57,6 +90,7 @@ class ContactFragment  : Fragment(), OnMapReadyCallback {
     override fun onResume() {
         super.onResume()
         googlemapView?.onResume()
+
     }
 
     override fun onPause() {
@@ -83,6 +117,21 @@ class ContactFragment  : Fragment(), OnMapReadyCallback {
         super.onLowMemory()
         googlemapView?.onLowMemory()
     }
+
+    override fun onClick(v: View?) {
+        val message: String = textFeedback.getText().toString()
+        auth = FirebaseAuth.getInstance()
+        val senderID = auth.currentUser?.uid
+        if (TextUtils.isEmpty(message)){
+            textFeedback.setError("Enter Your Message");
+            textFeedback.requestFocus();
+            return;
+        }
+
+        //database.child("Messages").child("senderID").child(senderID.toString()).child(message).push()
+        myRef.child("Messages").child("sender ID").child(senderID.toString()).push().setValue(message)
+    }
+
 
 
 
