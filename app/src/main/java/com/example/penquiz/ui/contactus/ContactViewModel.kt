334fe584e0b8dce94d6504768c61bc85f66cfbf1
aomsw13 <54433874+aomsw13@users.noreply.github.com>
2020-03-21 17:05:36 +0700

    update contactViewModel.kt
    Please reject this file (no longer use)

--- app/src/main/java/com/example/penquiz/ui/contactus/ContactViewModel.kt
+++ app/src/main/java/com/example/penquiz/ui/contactus/ContactViewModel.kt
@@ -1,12 +1,21 @@
 package com.example.penquiz.ui.contactus
 
+import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.ViewModel
+import com.example.penquiz.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 
 class ContactViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "Contact us"
     }
     val text: LiveData<String> = _text
+
+    private val mapFragment = SupportMapFragment()
+
+
 }
