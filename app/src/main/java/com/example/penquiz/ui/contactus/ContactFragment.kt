d432589ff7621948204ec443b33aea02a5ae3a41
aomsw13 <54433874+aomsw13@users.noreply.github.com>
2020-03-21 17:06:55 +0700

    update ContactFragment.kt
    add code for mapView and success to show map on application. However, I don't do "send us feedback" part yet.

--- app/src/main/java/com/example/penquiz/ui/contactus/ContactFragment.kt
+++ app/src/main/java/com/example/penquiz/ui/contactus/ContactFragment.kt
@@ -9,22 +9,82 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.penquiz.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
-class ContactFragment  : Fragment(){
+
+class ContactFragment  : Fragment(), OnMapReadyCallback {
     private lateinit var contactViewModel: ContactViewModel
+    private var googlemapView: MapView? = null
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        googlemapView?.onSaveInstanceState(outState)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_contactus,container,false)
+
         val textView: TextView = root.findViewById(R.id.text_contact)
         contactViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        googlemapView = root.findViewById(R.id.mapView)
+        googlemapView?.onCreate(savedInstanceState)
+        googlemapView?.getMapAsync(this);
+
         return root
-// Inflate the layout for this fragment
-//return inflater.inflate(R.layout.fragment_contactus, container, false)
+//// Inflate the layout for this fragment
+////return inflater.inflate(R.layout.fragment_contactus, container, false)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        googleMap.addMarker(MarkerOptions().position(LatLng(13.793128, 100.323322)).title("Mahidol University"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(13.793128, 100.323322),15.0f))
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        googlemapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        googlemapView?.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        googlemapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        googlemapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        googlemapView?.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        googlemapView?.onLowMemory()
     }
 
 
+
 }
+
