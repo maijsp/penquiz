d56dcbde26bc01606b2c5044771f6b3396bacebe
Saranphon Phaithoon <spak.flash12@gmail.com>
2020-04-04 18:34:00 +0700

    Update QuizActivity

---
+++ app/src/main/java/com/example/penquiz/Questions.kt
@@ -0,0 +1,10 @@
+package com.example.penquiz
+
+public class Questions {
+    var answer: String = ""
+    var option_A: String = ""
+    var option_B: String = ""
+    var option_C: String = ""
+    var option_D: String = ""
+    var questionText: String = ""
+}
