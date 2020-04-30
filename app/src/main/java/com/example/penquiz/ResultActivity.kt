d1a744391fe75d28255a40068927d55263b5d4c7
Saranphon Phaithoon <spak.flash12@gmail.com>
2020-05-01 02:27:39 +0700

    Show score at ResultActivity and improve design

--- app/src/main/java/com/example/penquiz/ResultActivity.kt
+++ app/src/main/java/com/example/penquiz/ResultActivity.kt
@@ -1,12 +1,22 @@
 package com.example.penquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        var score: Int = intent.getIntExtra("score", 0)
+        Log.i("SCORE", "Score receivce  : ${score}")
+        var scoreTextView = findViewById<TextView>(R.id.score)
+        scoreTextView.setText(score.toString())
     }
 }
