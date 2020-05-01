ccc246de517053ad4e6cbd29f7f72fb233c35618
Saranphon Phaithoon <spak.flash12@gmail.com>
2020-05-02 00:39:54 +0700

    Retrieve data to ArrayList<Quizes> for search features

--- app/src/main/java/com/example/penquiz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/penquiz/ui/home/HomeFragment.kt
@@ -9,27 +9,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.penquiz.MainActivity
 import com.example.penquiz.QuizActivity
 import com.example.penquiz.R
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.quickstart.database.kotlin.models.Quizes
-import kotlinx.android.synthetic.main.quizes_row.*
 
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private lateinit var mQuiz: Quizes
     private lateinit var mAuth: FirebaseAuth // reretrieve database authentication
     private lateinit var quizesRef: DatabaseReference
+    private lateinit var database: FirebaseDatabase
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var rootView: View
+
+    private lateinit var quizeList: ArrayList<Quizes> // to keep quiz data retrieve from firebase
 
     companion object {
         private const val TAG = "CheckDatabase"
@@ -43,34 +39,59 @@ class HomeFragment : Fragment() {
         mAuth = FirebaseAuth.getInstance()
         var currentUserID = mAuth.currentUser
 
-        quizesRef = FirebaseDatabase.getInstance().getReference().child("Quizes")
-        println(quizesRef)
-        val options: FirebaseRecyclerOptions<Quizes> = FirebaseRecyclerOptions.Builder<Quizes>().setQuery(quizesRef, Quizes::class.java).build()
-        val firebaseRecyclerAdapter = object : FirebaseRecyclerAdapter<Quizes, CustomViewHolder>(options) {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-                val itemview = LayoutInflater.from(parent.context).inflate(R.layout.quizes_row, parent, false)
-                return CustomViewHolder(itemview)
+//        quizesRef = FirebaseDatabase.getInstance().getReference().child("Quizes")
+//        println(quizesRef)
+//
+//        val options: FirebaseRecyclerOptions<Quizes> = FirebaseRecyclerOptions.Builder<Quizes>().setQuery(quizesRef, Quizes::class.java).build()
+//        val firebaseRecyclerAdapter = object : FirebaseRecyclerAdapter<Quizes, CustomViewHolder>(options) {
+//            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+//                val itemview = LayoutInflater.from(parent.context).inflate(R.layout.quizes_row, parent, false)
+//                return CustomViewHolder(itemview)
+//            }
+//            override fun onBindViewHolder(holder: CustomViewHolder, position: Int, quiz: Quizes) {
+//                val refid = getRef(position).key.toString()
+//                // Log.d("myTag", refid)
+//                quizesRef.child(refid).addValueEventListener(object: ValueEventListener {
+//                    override fun onCancelled(p0: DatabaseError) {
+//                        Log.e(TAG, "Fail to load database")
+//                    }
+//                    override fun onDataChange(p0: DataSnapshot) {
+//                        holder.quizname.setText(quiz.title)
+//                        holder.quiznum.setText(quiz.getNumQuestion())
+//                        println("$quiz_name and $quiz_detail")
+//                    }
+//                })
+//            }
+//        }
+//        // set layout
+//        mRecyclerView.layoutManager = LinearLayoutManager(activity)
+//        // set adapter
+//        mRecyclerView.adapter = firebaseRecyclerAdapter
+//        firebaseRecyclerAdapter.startListening()
+
+        // Fetching data from firebase
+        database = FirebaseDatabase.getInstance()
+        quizesRef = database.getReference()
+        quizeList = ArrayList()
+        quizesRef.child("Quizes").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
             }
-            override fun onBindViewHolder(holder: CustomViewHolder, position: Int, quiz: Quizes) {
-                val refid = getRef(position).key.toString()
-                // Log.d("myTag", refid)
-                quizesRef.child(refid).addValueEventListener(object: ValueEventListener {
-                    override fun onCancelled(p0: DatabaseError) {
-                        Log.e(TAG, "Fail to load database")
-                    }
-                    override fun onDataChange(p0: DataSnapshot) {
-                        holder.quizname.setText(quiz.title)
-                        holder.quiznum.setText(quiz.getNumQuestion())
-                        println("$quiz_name and $quiz_detail")
-                    }
-                })
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // get all children in this level
+                val children:Iterable<DataSnapshot> = dataSnapshot.children
+                // for each items
+                for (child:DataSnapshot in children) {
+                    val quize: Quizes? = child.getValue(Quizes::class.java)
+                    quizeList.add(quize!!)
+                    Log.d("fetching", "${quize!!.title} ${quize!!.num}")
+                }
+                done(quizeList)
             }
-        }
-        // set layout
-        mRecyclerView.layoutManager = LinearLayoutManager(activity)
-        // set adapter
-        mRecyclerView.adapter = firebaseRecyclerAdapter
-        firebaseRecyclerAdapter.startListening()
+
+        })
+
+
         return rootView
     }
     inner class CustomViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -86,5 +107,15 @@ class HomeFragment : Fragment() {
                 itemView.context.startActivity(intent)
             }
         }
+    }
+
+    fun done(k: ArrayList<Quizes>?) {
+        Log.d("fetching", "result : ${k}}")
+        if (k != null) {
+            for (quiz:Quizes in k) {
+                Log.d("fetching", "Quiz : ${quiz.title} and ${quiz.num}")
+            }
+        }
+        Log.d("fetching", "Success retreieve database")
     }
 }
