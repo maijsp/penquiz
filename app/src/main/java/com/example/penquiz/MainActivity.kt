cc927128fee7f6e4d73aed7bca5a8b84955dbf60
aomsw13 <54433874+aomsw13@users.noreply.github.com>
2020-05-02 22:08:31 +0700

    Update MainActivity.kt
    change code to display user's image and user's email in nav_header

--- app/src/main/java/com/example/penquiz/MainActivity.kt
+++ app/src/main/java/com/example/penquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.penquiz
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -13,15 +14,20 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var auth: FirebaseAuth
+    lateinit var mDatabase: DatabaseReference
     // Access a Cloud Firestore instance from your Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +44,31 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
 
+        savedInstanceState?.let { onRestoreInstanceState(it) }
+        var user: FirebaseUser? =FirebaseAuth.getInstance().currentUser
+        mDatabase = FirebaseDatabase.getInstance().getReference("image")
+        mDatabase!!.keepSynced(true)
+        mDatabase.child(user!!.uid.toString()).addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                // No Need To implement we don't use it
+                // Because we get only image not edit or else
+                // I dunno no
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // get value by using string from java class
+                // Because image is retrieve from Firebase
+                // URI already include in firebase
+                // we use this URI for each user
+                // Yes still there
+                val img_circle : ImageView = findViewById(R.id.camera_image) as ImageView
+                val image = snapshot.child("image").getValue(String::class.java)
+                Picasso.get()
+                    .load(image)
+                    .into(img_circle)
+                user_email.text = user!!.email
+            }
+        })
+//        nav_header_main
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -60,5 +91,13 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var user: FirebaseUser? =FirebaseAuth.getInstance().currentUser
+        outState.putParcelable("outputFileUri", user?.photoUrl)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.getString("outputFileUri")
+    }
 }
