18f876a5b8fb6efc2de53e81b8b85c783eaa9f33
Saranphon Phaithoon <spak.flash12@gmail.com>
2020-05-03 15:55:49 +0700

    create icon of history

--- app/src/main/java/com/example/penquiz/MainActivity.kt
+++ app/src/main/java/com/example/penquiz/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_tools, R.id.nav_share, R.id.nav_send),
+            setOf(R.id.nav_home, R.id.nav_history, R.id.nav_contact, R.id.nav_profile),
             drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
